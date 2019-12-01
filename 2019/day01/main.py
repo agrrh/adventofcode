91f00c63db52ed85ae7e000b615f3a04485ca87c
Kirill K <kovalev.kirill.a@gmail.com>
2019-12-01 19:47:43 +0300

    chore: add day01
    Signed-off-by: Kirill K <kovalev.kirill.a@gmail.com>

---
+++ 2019/day01/main.py
@@ -0,0 +1,34 @@
+def fuel_reqs(m):
+    r = m / 3 - 2
+    return r if r > 0 else 0
+
+
+def fuel_reqs_recursive(m):
+    s = 0
+    while fuel_reqs(m) > 0:
+        s += fuel_reqs(m)
+        m = fuel_reqs(m)
+    return s
+
+
+if __name__ == '__main__':
+    with open('input.txt') as fp:
+        data = fp.readlines()
+
+    data = [int(_.strip()) for _ in data]
+
+    r = [
+        fuel_reqs(module_mass)
+        for module_mass
+        in data
+    ]
+
+    print('sum of the fuel requirements for all of the modules: {}'.format(sum(r)))
+
+    r = [
+        fuel_reqs_recursive(module_mass)
+        for module_mass
+        in data
+    ]
+
+    print('sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel: {}'.format(sum(r)))
